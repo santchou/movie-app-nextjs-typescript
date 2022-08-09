894e2cafff9463f82076ffb65062b2089f90281a
gsantchoukenfack@gmail.com <gsantchoukenfack@gmail.com>
2022-08-09 12:43:08 +0100

    add progress bar and mofified scrollbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
